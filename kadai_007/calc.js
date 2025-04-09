55bfd7bb35f7c8a8becf7c9fe4324b324634a536
Nozomi24Nishimura <166346705+Nozomi24Nishimura@users.noreply.github.com>
2025-04-09 14:39:38 +0900

    kadai_007　提出

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+
+
+// 変数numに1以上の正の数を代入して、プログラム内で使用してください。
+let num = 65;
+
+if (num % 3 == 0 && num % 5 == 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 == 0) {
+  console.log("3の倍数です");
+} else if (num % 5 == 0) {
+  console.log("5の倍数です");
+}else{
+    console.log(num);
+}
+
+
